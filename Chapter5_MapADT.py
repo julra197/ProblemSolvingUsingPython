062b6814082df121166f6dfbd2f3a9d5211a6789
julra197 <ralfebert@outlook.com>
2017-11-11 15:36:12 +0100

    Test code added

--- Chapter5_MapADT.py
+++ Chapter5_MapADT.py
@@ -57,3 +57,22 @@ class HashTable:
     def __setitem__(self,key,data):
           self.put(key,data)
           
+#Code to test the implementation:
+H=HashTable()
+H[54]="cat"
+H[26]="dog"
+H[93]="lion"
+H[17]="tiger"
+H[77]="bird"
+H[31]="cow"
+H[44]="goat"
+H[55]="pig"
+H[20]="chicken"
+print(H.slots)
+print(H.data)
+print(H[20])
+print(H[17])
+H[20]='duck'
+print(H[20])
+print(H.data)
+print(H[99])
