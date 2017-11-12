fe9056d5b59d15601693ab384f7cb9991f19bfd9
julra197 <ralfebert@outlook.com>
2017-11-12 11:47:29 +0100

    Ex 6 added

--- Chapter5_MapADT.py
+++ Chapter5_MapADT.py
@@ -5,28 +5,38 @@ class HashTable:
         self.size = 11
         self.slots = [None] * self.size
         self.data = [None] * self.size
-        
+    
+    # Exercise 7 Reimplement put so that the table will automatically resize itself when the loading factor
+    # reaches a predefined value
+    
     def put(self,key,data):
-      hashvalue = self.hashfunction(key,len(self.slots))
+        hashvalue = self.hashfunction(key,len(self.slots))
+      
+        if self.loadingfactor()>0.8:
+            pass
+            #TODO
     
-      if self.slots[hashvalue] == None:
-        self.slots[hashvalue] = key
-        self.data[hashvalue] = data
-      else:
-        if self.slots[hashvalue] == key:
-          self.data[hashvalue] = data  #replace
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
         else:
-          nextslot = self.rehash(hashvalue,len(self.slots))
-          while self.slots[nextslot] != None and \
+            if self.slots[hashvalue] == key:
+                self.data[hashvalue] = data  #replace
+            else:
+                nextslot = self.rehash(hashvalue,len(self.slots))
+            while self.slots[nextslot] != None and \
                           self.slots[nextslot] != key:
-            nextslot = self.rehash(nextslot,len(self.slots))
+                nextslot = self.rehash(nextslot,len(self.slots))
     
-          if self.slots[nextslot] == None:
-            self.slots[nextslot]=key
-            self.data[nextslot]=data
-          else:
-            self.data[nextslot] = data #replace
+            if self.slots[nextslot] == None:
+                self.slots[nextslot]=key
+                self.data[nextslot]=data
+            else:
+                self.data[nextslot] = data #replace
     
+    def loadingfactor(self):
+        return len(self)/self.size
+                
     def hashfunction(self,key,size):
          return key%size
     
@@ -68,8 +78,24 @@ class HashTable:
     # Exercise 5: Implement the in method (__contains__):
     def __contains__(self, item):
         return (item in self.data)
+    
+    # Exercise 6: Implement the del method:
+    def __delitem__(self, key):
+        searchslot = self.hashfunction(key, len(self.slots))
+        stop = False
+        found = False
+        position = searchslot
+        while not found and not stop:
+            if self.slots[position] == key:
+                self.slots[position] = None
+                self.data[position] = None
+                found = True
+            else:
+                position=self.rehash(position, len(self.slots))
+                if position == searchslot:
+                    stop = True
           
-#Code to test the implementation:
+# Code to test the implementation:
 H=HashTable()
 H[54]="cat"
 H[26]="dog"
@@ -94,3 +120,11 @@ print("Testing the in funcion:")
 print("\'tiger\' is in H:", 'tiger' in H)
 print("None is in H:", None in H)
 print("\'NotinH\' should evaluate to false:", 'notInH' in H)
+print("Test the del function:")
+print("Slots:", H.slots)
+print("Data:", H.data)
+del H[55]
+del H[43]
+print("Slots after deletion:", H.slots)
+print("Data after deletion:", H.data)
+print("loading Factor", H.loadingfactor())
